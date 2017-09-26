dbd28567e1a9915aa56fa1766bb0f2cb0a079e00
Admin-01-PC\Admin-01 <liuzhaoxuchn@163.com>
2017-09-26 16:01:02 +0800

    阻塞式，由原来的白手环修改

---
+++ black_band-1/USER/main.c
@@ -0,0 +1,212 @@
+#include "sys.h"
+#include "usart3.h"
+#include "usart.h"
+#include "led.h"
+#include "delay.h"
+#include "emg.h"
+#include "exti.h"
+#include "key.h"
+#include "sram.h"
+//#include "config.h"
+//#include "hc05.h"
+//作者：jeremyxu
+u8 predict[1000]={0};
+u8 motiondetect=0;
+int main(void)
+{
+
+  u16 i =0;;
+ 
+  delay_init(168);      //初始化延时函数
+  FSMC_SRAM_Init();	    //初始化SRAM
+  initiate_filter();
+  delay_ms(1000);		//等待蓝牙模块上电稳定
+  LED_Init();				  //初始化LED端口  
+  NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);//设置系统中断优先级分组2
+  EXTIX_Init();         //外部中断使能
+  usart3_init(460800);	//初始化串口3为:460800,波特率. 
+  uart_init(460800);	//串口初始化波特率为115200
+	
+	{
+		LED3=0;delay_ms(100);LED4=0;delay_ms(100);LED5=0;delay_ms(100);
+	  LED3=1;delay_ms(100);LED4=1;delay_ms(100);LED5=1;delay_ms(100);
+		printf("begin\r\n");
+	}
+  while(1)
+
+  {
+		if(Receive_complete_Flag)
+		{
+		 Receive_complete_Flag = 0;
+	   Data_12_8_transform();		
+		}
+		
+		
+		
+    if(trainig_flag ==1)
+	{   
+		   
+		  //trainig_flag=0;
+		if(data_full_flag==1)    
+		{   
+			data_full_flag=0;
+			if((count_temp+3)%50>=9)//count_temp=6-46, 56-96.............
+			{ 
+			  feature_extraction();
+			  for(i=0;i<32;i++)
+			  {
+				feat_matrix[window_count][i]=feat[i];
+				
+			  }
+			  window_count++;			  
+			}							
+		} 	  
+       if(window_count==410&&dur==0)	
+		    LDA_train(feat_matrix);
+	}
+  if(testing_flag==1)
+  {
+	  
+	  if(data_full_flag==1)    
+	  {
+		  data_full_flag=0;
+	    feature_extraction();
+		predict[count%1000]=LDA_predict(feat);	
+		  motiondetect=predict[count%1000];
+		  if(count>4&&predict[count%1000]==predict[(count-1)%1000]&&
+		  predict[(count-1)%1000]==predict[(count-2)%1000]&&
+	      predict[(count-2)%1000]==predict[(count-3)%1000]&&
+	      predict[(count-3)%1000]==predict[(count-4)%1000]&&
+	      predict[(count-4)%1000]==predict[(count-5)%1000])
+	     {
+		  switch(motiondetect)
+		  {
+			case 0:
+			  USART_SendData(USART1, 'N');
+			  //模式1
+		    {LED3=1;LED4=1;LED5=1;LED3=0;LED4=1;PFout(7)=1;}
+			  break;
+			case 1:
+			  USART_SendData(USART1, 'P');
+			  //模式2
+		    {LED3=1;LED4=1;PFout(7)=1;LED3=1;LED4=0;LED5=1;}
+			  break;
+			case 2:
+			  USART_SendData(USART1, 'R');
+		    //模式3
+		    {LED3=1;LED4=1;LED5=1;LED3=0;LED4=0;LED5=1;}
+			  break;
+			case 3:
+			  USART_SendData(USART1, 'T');
+			  //模式4
+		    {LED3=1;LED4=1;LED5=1;LED3=1;LED4=1;LED5=0;}
+			  break;
+			case 4:
+			  USART_SendData(USART1, 'B');
+			  //模式5
+		    {LED3=1;LED4=1;LED5=1;LED3=0;LED4=1;LED5=0;}
+			  break;  
+			case 5:
+			  USART_SendData(USART1, '0');
+			  //模式6
+		    {LED3=1;LED4=1;LED5=1;LED3=1;LED4=0;LED5=0;}
+			  break;
+		    default:			
+		      break;
+		  }
+	    }
+		  if(count ==409)
+		  {   
+			  
+			  LED0 =0;//提示数据结束了，等待下一次按键
+				LED3=0;delay_ms(100);LED4=0;delay_ms(100);LED5=0;delay_ms(100);
+	      LED3=1;delay_ms(100);LED4=1;delay_ms(100);LED5=1;delay_ms(100);
+				printf("count = 409\r\n");
+			  
+		  }
+		  
+	  }	    	  	
+  }
+  
+  
+  }
+}
+
+
+////////////////////////////////////////////////
+
+
+
+//		//全关
+
+//		LED3=1;
+//		LED4=1;
+//		LED5=1;
+
+//		//模式1
+//		LED3=1;
+//		LED4=1;
+//		LED5=1;
+//		LED3=0;
+//		LED4=1;
+//		LED5=1;
+//		delay_ms(1000);
+//		//模式2
+//		LED3=1;
+//		LED4=1;
+//		LED5=1;
+//		LED3=1;
+//		LED4=0;
+//		LED5=1;
+//		delay_ms(1000);
+//		//模式3
+//		LED3=1;
+//		LED4=1;
+//		LED5=1;
+//		LED3=0;
+//		LED4=0;
+//		LED5=1;
+//		delay_ms(1000);
+//		//模式4
+//		LED3=1;
+//		LED4=1;
+//		LED5=1;
+//		LED3=1;
+//		LED4=1;
+//		LED5=0;
+//		delay_ms(1000);
+//		//模式5
+//		LED3=1;
+//		LED4=1;
+//		LED5=1;
+//		LED3=0;
+//		LED4=1;
+//		LED5=0;
+//		delay_ms(1000);
+//		//模式6
+//		LED3=1;
+//		LED4=1;
+//		LED5=1;
+//		LED3=1;
+//		LED4=0;
+//		LED5=0;
+//		delay_ms(1000);
+//	
+//       //提示
+//		LED3=0;
+//		delay_ms(100);
+//		LED4=0;
+//		delay_ms(100);
+//		LED5=0;
+//		delay_ms(100);
+//		LED3=1;
+//		delay_ms(100);
+//		LED4=1;
+//		delay_ms(100);
+//		LED5=1;
+//		delay_ms(100);
+
+
+
+
+
